95ada42e95d95e9853787905af047d1666307e17
xeno_xenia <vorobeva.ksenia.and@gmail.com>
2021-05-10 16:43:37 +0300

    сделана 1 страница, без адаптивности

---
+++ for_js.js
@@ -0,0 +1,25 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName('slide');
+  let slides_text = document.getElementsByClassName('slide_text')
+  if (n > slides.length){
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+      slides_text[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+  slides_text[slideIndex-1].style.display = "inline-block";
+}
